6293ce77fe83f1dbca3cc7118318bdda118cdd5f
Tigay Aleksandr <tevikfxtigay@yandex.ru>
2023-07-20 21:23:22 +0300

    added nexign website test

---
+++ src/test/java/com/nexign/job/pages/MainPage.java
@@ -0,0 +1,47 @@
+package com.nexign.job.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MainPage {
+    private final SelenideElement
+            vacancyInput = $("ul.header-new__list"),
+            searchVacancyInput = $(".page-content"),
+            actualVacancyInput = $(".view-content :nth-child(4) a"),
+            clickSendResumeInput = $("#block-system-main button"),
+            checkSubmissionInput = $(".form-wrapper__content");
+
+    public MainPage openPage() {
+        open("https://job.nexign.com/");
+        return this;
+    }
+
+    public MainPage setVacancyInput(String value) {
+        vacancyInput.shouldHave(text(value)).click();
+        return this;
+    }
+
+    public MainPage setSearchVacancyInput(String value) {
+        searchVacancyInput.$(byLinkText(value)).click();
+        return this;
+    }
+
+    public MainPage setActualVacancyInput(String value) {
+        actualVacancyInput.shouldHave(text(value)).click();
+        return this;
+    }
+
+    public MainPage setClickSendResumeInput(String value) {
+        clickSendResumeInput.shouldHave(text(value)).click();
+        return this;
+    }
+
+    public MainPage setCheckSubmissionInput(String value) {
+        checkSubmissionInput.shouldHave(text(value));
+        return this;
+    }
+
+}
---
+++ src/test/java/com/nexign/job/tests/SearchTest.java
@@ -0,0 +1,50 @@
+package com.nexign.job.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Поиск на странице job.nexign.com")
+public class SearchTest extends TestBase {
+    private final static String vacancy = "Вакансии";
+    private final static String searchVacancy = "Тестирование";
+    private final static String actualVacancy = "QA Automation engineer";
+    private final static String clickSendResume = "Откликнуться";
+    private final static String checkSubmission = "Пришли своё резюме";
+    private final static String textSelection = "\"";
+
+    @BeforeEach
+    public void openCheckPage() {
+        step("Открываем страницу job.nexign.com", () -> {
+            mainPage.openPage();
+        });
+    }
+
+    @Test
+    @Tag("UITest")
+    @DisplayName("Поиск конкретной вакансии")
+    void vacancySearchTest() {
+        step(String.format("Нажимаем на вкладку %s", textSelection + vacancy + textSelection), () -> {
+            mainPage.setVacancyInput(vacancy);
+        });
+
+        step(String.format("Нажимаем на раздел %s", textSelection + searchVacancy + textSelection), () -> {
+            mainPage.setSearchVacancyInput(searchVacancy);
+        });
+
+        step(String.format("Нажимаем на вакансию %s", textSelection + actualVacancy + textSelection), () -> {
+            mainPage.setActualVacancyInput(actualVacancy);
+        });
+
+        step(String.format("Нажимаем на кнопку %s", textSelection + clickSendResume + textSelection), () -> {
+            mainPage.setClickSendResumeInput(clickSendResume);
+        });
+
+        step(String.format("Переход на форму %s", textSelection + checkSubmission + textSelection), () -> {
+            mainPage.setCheckSubmissionInput(checkSubmission);
+        });
+    }
+}
